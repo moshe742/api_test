1a7fabc7025522cf846ab35ae2b9e7e726c1ae57
Moshe Nahmias <moshegrey@gmail.com>
2020-10-13 13:12:16 +0300

    add code to setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    maintainer="Moshe Nahmias",
+    maintainer_email="maintainer@example.com",
+    name="open-weather-client",
+    version="0.1",
+    packages=find_packages(),
+    url="URL",
+    python_requires=">=3.6"
+)
